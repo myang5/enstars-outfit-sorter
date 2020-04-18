ea19d28be7105f5f3056cb19013e760faf82ad99
Midori Yang <myang5@wellesley.edu>
2020-04-17 22:21:15 -0400

    Updates

---
+++ precache-manifest.8d793752227e327c87162a93064d8f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8378bd7cde6465c530bab78d7f1a826f",
+    "url": "/enstars-outfit-sorter/index.html"
+  },
+  {
+    "revision": "9271981dcfe5647202c6",
+    "url": "/enstars-outfit-sorter/static/css/main.08593e67.chunk.css"
+  },
+  {
+    "revision": "a5ca4d4761b01a040cfb",
+    "url": "/enstars-outfit-sorter/static/js/2.dcd36a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/enstars-outfit-sorter/static/js/2.dcd36a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9271981dcfe5647202c6",
+    "url": "/enstars-outfit-sorter/static/js/main.b275bf9a.chunk.js"
+  },
+  {
+    "revision": "c40e28794536246050bb",
+    "url": "/enstars-outfit-sorter/static/js/runtime-main.78dbdcb7.js"
+  }
+]);
