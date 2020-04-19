81c9b499cf5a654ef1919444383fe40e70d67904
Midori Yang <myang5@wellesley.edu>
2020-04-19 17:02:35 -0400

    implemented load more on scroll without throttle

--- src/OutfitList.js
+++ src/OutfitList.js
@@ -8,92 +8,108 @@ export default class OutfitList extends React.Component {
     super(props);
     this.state = {
       error: false,
-      isLoaded: false,
+      isLoading: false,
+      hasMore: true,
+      status: '',
       outfits: [],
       loadedOutfits: [],
-      status: ''
+      outfitsToLoad: 20,
     };
-    // window.onscroll = throttle(() => {
-    //   const {
-    //     loadUsers,
-    //     state: {
-    //       error,
-    //       isLoading,
-    //       loadedOutfits,
-    //     },
-    //   } = this;
-
-    //   // Bails early if:
-    //   // * there's an error
-    //   // * it's already loading
-    //   // * there's nothing left to load
-    //   if (error || isLoading || !loadedOutfits.length) return;
-
-    //   // Checks that the page has scrolled to the bottom
-    //   if (
-    //     window.innerHeight + document.documentElement.scrollTop
-    //     === document.documentElement.offsetHeight
-    //   ) {
-    //     loadOutfits();
-    //   }
-    // }, 100);
+    this.onScroll = this.onScroll.bind(this);
+    this.loadOutfits = this.loadOutfits.bind(this);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.query !== prevProps.query) {
-      this.setState({ isLoaded: false }, () => {
+      this.setState({ status: 'Loading...' }, () => {
         const sheetId = 'Stat Bonuses';
         fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetId}?key=${apiKey}`)
           .then(res => res.json())
           .then(res => {
             let outfits = filterGsData(res, { query: this.props.stringQuery, isInclusive: this.props.isInclusive }); //all outfits that match character/outfit (inclusive)
-              //console.log(outfits);
-              if (this.props.selAttr.size > 0) {
-                for (let i = outfits.length - 1; i >= 0; i--) {
-                  let queried = false;
-                  for (let attr of this.props.selAttr) {
-                    if (outfits[i][attr.toLowerCase()]) { //inclusive search by attr
-                      queried = true;
-                      break;
-                    }
-                    // if (!outfits[i][attr.toLowerCase()]) { //currently only keeps outfits that meet all attr criteria (exclusive)
-                    //   outfits.splice(i, 1);
-                    //   break;
-                    // }
-                  }
-                  if (!queried) outfits.splice(i, 1);
-                }
-              }
-              outfits.sort((a, b) => { //sort from highest to lowest queried stat bonus
-                let totalBonusA = 0;
-                let totalBonusB = 0;
+            //console.log(outfits);
+            if (this.props.selAttr.size > 0) {
+              for (let i = outfits.length - 1; i >= 0; i--) {
+                let queried = false;
                 for (let attr of this.props.selAttr) {
-                  totalBonusA += a[attr.toLowerCase()];
-                  totalBonusB += b[attr.toLowerCase()];
+                  if (outfits[i][attr.toLowerCase()]) { //inclusive search by attr
+                    queried = true;
+                    break;
+                  }
+                  // if (!outfits[i][attr.toLowerCase()]) { //currently only keeps outfits that meet all attr criteria (exclusive)
+                  //   outfits.splice(i, 1);
+                  //   break;
+                  // }
                 }
-                return totalBonusB - totalBonusA;
-              })
-              return { isLoaded: true, outfits: outfits, status: `${outfits.length} outfits found` }
+                if (!queried) outfits.splice(i, 1);
+              }
+            }
+            outfits.sort((a, b) => { //sort from highest to lowest queried stat bonus
+              let totalBonusA = 0;
+              let totalBonusB = 0;
+              for (let attr of this.props.selAttr) {
+                totalBonusA += a[attr.toLowerCase()];
+                totalBonusB += b[attr.toLowerCase()];
+              }
+              return totalBonusB - totalBonusA;
+            })
+            return { outfits: outfits, loadedOutfits: outfits.slice(0, this.state.outfitsToLoad), status: `${outfits.length} outfits found` }
           })
           .then(res => this.setState(res));
       });
     }
   }
 
+  //modified from this tutorial: https://alligator.io/react/react-infinite-scroll/
+  onScroll(event) {
+      const {
+        state: {
+          isLoading,
+          hasMore,
+        },
+      } = this;
+
+      // Bails early if:
+      // * there's an error
+      // * it's already loading
+      // * there's nothing left to load
+      if (isLoading || !hasMore) { console.log('not loadin'); return; }
+
+      // Checks that the page has scrolled to the bottom
+      if (
+        window.innerHeight + event.target.scrollTop
+        >= event.target.scrollHeight - 500
+      ) {
+        console.log('loading more');
+        this.loadOutfits();
+      }
+  }
+
+  loadOutfits() {
+    this.setState({ isLoading: true }, () => {
+      this.setState((state, props) => {
+        // console.log(state.outfits.slice(state.loadedOutfits.length, state.outfitsToLoad));
+        // console.log()
+        let loadedOutfits = state.loadedOutfits;
+        loadedOutfits = loadedOutfits.concat(state.outfits.slice(state.loadedOutfits.length, state.loadedOutfits.length + state.outfitsToLoad));
+        const hasMore = loadedOutfits.length < state.outfits.length;
+        return { hasMore: hasMore, isLoading: false, loadedOutfits: loadedOutfits  };
+      });
+    })
+  }
+
   render() {
-    let body = null;
-    if (!this.state.isLoaded) {
-      body = <p className='status'>Loading...</p>
-    } else {
-      const outfits = this.state.outfits.map((elt) =>
-        <Outfit key={elt.character + elt.outfit} props={elt} attributes={this.props.attributes} selAttr={this.props.selAttr} />
-      );
-      body = (<><p className='status'>{this.state.status}</p>{outfits}</>)
-    }
+    const outfits = this.state.loadedOutfits.map((elt) =>
+      <Outfit key={elt.character + elt.outfit} props={elt} attributes={this.props.attributes} selAttr={this.props.selAttr} />
+    );
     // console.log('finished loading outfit list', performance.now())
     return (
-      <div className='outfitList'>
-        {body}
+      <div className='outfitList' onScroll={this.onScroll}>
+        <p className='status'>{this.state.status}</p>
+        {outfits}
+        {!this.state.hasMore &&
+          <p className='status'>End of results</p>
+        }
       </div>
     )
   }
